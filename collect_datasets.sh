bfd9f644404245748ab1f8c56bf4ddac58dc6da7
uzair789 <uzair.789@hotmail.com>
2021-03-13 03:12:16 -0500

    processing 200 training episodes for palletizing

--- collect_datasets.sh
+++ collect_datasets.sh
@@ -1,12 +1,13 @@
 
 tasks=('towers-of-hanoi' 'sweeping-piles' 'stack-block-pyramid' 'place-red-in-green' 'packing-boxes' 'manipulating-rope' 'align-box-corner' 'assembling-kits')
+tasks=('palletizing-boxes')
 i=1
 for task in ${tasks[@]}; do
 	
         echo " $i/8 processing for $task"
-	python demos.py --assets_root=./ravens/environments/assets/ --disp=True --task=$task --mode=train --n=10
+	python demos.py --assets_root=./ravens/environments/assets/ --disp=True --task=$task --mode=train --n=200
 	echo "train done"
-        python demos.py --assets_root=./ravens/environments/assets/ --disp=True --task=$task --mode=test --n=100
+        # python demos.py --assets_root=./ravens/environments/assets/ --disp=True --task=$task --mode=test --n=100
         echo "test done"
 	echo "--"
 	i=$(($i+1))
