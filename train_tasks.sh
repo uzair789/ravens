563f62d0e51a2cf3667567054ffa9b4ac10399f4
uzair789 <uzair.789@hotmail.com>
2021-03-13 03:15:23 -0500

    training palletizig for 200 episodes instead of hte 10 earlier

--- train_tasks.sh
+++ train_tasks.sh
@@ -1,11 +1,12 @@
 
 # tasks=('towers-of-hanoi' 'sweeping-piles' 'stack-block-pyramid' 'place-red-in-green' 'packing-boxes' 'manipulating-rope' 'align-box-corner' 'assembling-kits')
-tasks=('sweeping-piles' 'stack-block-pyramid' 'place-red-in-green' 'packing-boxes' 'manipulating-rope' 'align-box-corner' 'assembling-kits')
+# tasks=('sweeping-piles' 'stack-block-pyramid' 'place-red-in-green' 'packing-boxes' 'manipulating-rope' 'align-box-corner' 'assembling-kits')
+tasks=('palletizing-boxes')
 i=1
 for task in ${tasks[@]}; do
 	
         echo " $i/7 processing for $task"
-        python train.py --task=$task --agent=transporter --n_demos=10 --data_dir data
+        python train.py --task=$task --agent=transporter --n_demos=200 --data_dir data
 
 	echo "train done"
 	echo "--"
